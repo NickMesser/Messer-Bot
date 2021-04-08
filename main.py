3ac543aa269876bfdfb830d1695d006b68afdb94
Nick Messer <>
2021-04-08 20:54:24 +0000

    misc token stuff

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import os
 import sys
 from discord.ext import commands
 
+
 def json_exist(file_name):
     return os.path.exists(file_name)
 
+
 def load_data_file():
     if not json_exist('data.json'):
-        data = {'BotToken' : '', 'BotPrefix':'!!'}
+        data = {'BotToken': '', 'BotPrefix': '!!'}
         with open('data.json', 'w') as newfile:
             json.dump(data, newfile, sort_keys=True, indent=4)
         sys.exit('Please add bot token and bot prefix to data.json')
@@ -17,19 +19,30 @@ def load_data_file():
     with open("data.json") as f:
         return json.load(f)
 
+
 data = load_data_file()
 
 bot = commands.Bot(command_prefix=data['BotPrefix'])
 
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+
 
 @commands.command
 async def reload(ctx):
     bot.load_extension('cogs.suggest')
     ctx.send('Reloaded commands...')
 
+
 bot.load_extension('cogs.suggest')
 
-bot.run(data['BotToken'])
+if data['BotToken'] == '':
+  token = os.getenv('TOKEN')
+else:
+  token = data['BotToken']
+
+print(token)
+
+bot.run(token)
